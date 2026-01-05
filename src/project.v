33161bfcceac05efa90c09deba91722b623ddd55
Christian Hoene <christian.hoene@symonics.com>
2026-01-05 14:17:30 +0000

    wire up single modules into project

--- src/project.v
+++ src/project.v
@@ -24,4 +24,85 @@ module tt_um_hoene_firsttry (
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
+
+  // ui0 input_selector_in0;
+  // ui1 input_selector_in1;
+
+
+  // wire up the signals of input_selector
+
+  reg  input_selector_testmode;  // TODO
+  wire input_selector_out;
+  wire input_selector_in0selected;
+
+  tt_um_hoene_input_selector all_input_selector (
+      .in0        (ui_in[0]),
+      .in1        (ui_in[1]),
+      .testmode   (input_selector_testmode),
+      .clk        (clk),                        // clock
+      .rst_n      (rst_n),                      // not reset
+      .out        (input_selector_out),
+      .in0selected(input_selector_in0selected)
+  );
+
+  // wire up the signals of low pass filter
+  wire low_pass_filter_out;
+
+  tt_um_hoene_low_pass_filter all_low_pass_filter (
+      .in   (input_selector_out),
+      .clk  (clk),                 // clock
+      .rst_n(rst_n),               // not reset
+      .out  (low_pass_filter_out)
+  );
+
+  // wire up the signals of Manchester decoder
+  wire manchester_decoder_out_data;
+  wire manchester_decoder_out_clk;
+  wire manchester_decoder_out_error;
+  wire [5:0] manchester_decoder_out_pulsewidth;
+
+  tt_um_hoene_manchester_decoder user_manchester_decoder (
+      .in            (low_pass_filter_out),
+      .clk           (clk),                               // clock
+      .rst_n         (rst_n),                             // not reset
+      .out_data      (manchester_decoder_out_data),
+      .out_clk       (manchester_decoder_out_clk),
+      .out_error     (manchester_decoder_out_error),
+      .out_pulsewidth(manchester_decoder_out_pulsewidth)
+  );
+
+  // wire up the signals of protocol insync module
+  wire protocol_insync_out;
+
+  tt_um_hoene_protocol_insync user_protocol_insync (
+      .in_data (manchester_decoder_out_clk),
+      .in_clk  (manchester_decoder_out_clk),
+      .in_error(manchester_decoder_out_error),
+      .rst_n   (rst_n),
+      .clk     (clk),
+      .insync  (protocol_insync_out)
+  );
+
+
+  // wire up the signals of led PWM 
+  reg [9:0] led_pwm_data_red;
+  reg [9:0] led_pwm_data_green;
+  reg [9:0] led_pwm_data_blue;
+  wire led_pwm_out_red;
+  wire led_pwm_out_green;
+  wire led_pwm_out_blue;
+
+  tt_um_hoene_led_pwm user_led_pwm (
+      .data_red  (led_pwm_data_red),
+      .data_green(led_pwm_data_green),
+      .data_blue (led_pwm_data_blue),
+      .rst_n     (rst_n),
+      .clk       (clk),
+      .out_red   (led_pwm_out_red),
+      .out_green (led_pwm_out_green),
+      .out_blue  (led_pwm_out_blue)
+  );
+
+
+
 endmodule
