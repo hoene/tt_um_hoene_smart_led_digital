db0ad5317a6b4abb3a301e8995f0d3dd4f40c532
Christian Hoene <christian.hoene@symonics.com>
2026-03-05 17:33:44 +0000

    added pwm test

--- test/test_single_chip.py
+++ test/test_single_chip.py
@@ -74,11 +74,11 @@ class Helpers:
             )
 
     def pwm_decode(self):
-        # Counts the 16384 last IO outputs
+        # Counts the 1024 last IO outputs
         self.pwm_buffer.append(self.dut.uio_out.value)
 
         # Calculate the duty cycle for each LED
-        if len(self.pwm_buffer) > 16384:
+        if len(self.pwm_buffer) > 2048:
             last = self.pwm_buffer.pop(0)
             if last[4] == Logic(1):
                 self.led_red -= 1
@@ -478,7 +478,7 @@ async def test_protocol_bin(dut):
         assert dut.uo_out.value[7] == parity  # PWM_SET
 
 
-@cocotb.test()
+# @cocotb.test()
 async def test_protocol_din(dut):
 
     dut._log.info("Start protocol din")
@@ -534,3 +534,66 @@ async def test_protocol_din(dut):
         assert dut.user_project.protocol_state.value == 3
         assert dut.uio_out.value[3] == 1 - parity  # data output inverse
         assert dut.uo_out.value[7] == parity  # PWM_SET
+
+
+@cocotb.test()
+async def test_serial2parallel(dut):
+
+    dut._log.info("Start protocol din")
+
+    # Set the clock period to 41.67 ns (24 MHz)
+    clock = Clock(dut.clk, 42, unit="ns")
+    cocotb.start_soon(clock.start())
+
+    helpers = Helpers(dut, "serial2parallel")
+
+    # Reset
+    dut._log.info("Reset")
+    dut.ena.value = 1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+
+    await helpers.n_clock(10)
+    dut.rst_n.value = 1
+    dut._log.info("test protocol din")
+    ## now send one long impulses and two ones
+    data = [0, 1] * 64 + [1]
+    await helpers.manchester_encode(data, speed=19, pin=0)
+
+    assert dut.uo_out.value[4] == 0  # no error
+    assert helpers.decoder_buffer[-2:] == [1, 1]  # first bit is lost
+    assert dut.uo_out.value[5] == 1  # frame
+    assert dut.user_project.counters_bits.value == 0
+    assert dut.user_project.protocol_state.value == 0
+    assert dut.user_project.serial2parallel_data.value == 0
+
+    helpers.decoder_buffer = []
+    data = [1] * 12 + [0] * 14 + [1] * 6
+    await helpers.manchester_encode(data, speed=19, pin=0)
+    assert helpers.decoder_buffer == data
+    assert dut.user_project.counters_bits.value == 0
+    assert dut.user_project.protocol_state.value == 3
+    assert dut.uio_out.value[3] == 0
+    assert dut.uo_out.value[7] == 1  # PWM_SET
+    assert dut.user_project.serial2parallel_data.value == 0
+    assert dut.uio_out.value[4] == 0  # manchester encoder error
+
+    for wait in range(0, 50):
+        await helpers.n_clock(1)
+        helpers.log_outputs()
+    assert dut.uio_out.value[4] == 0  # manchester encoder error
+    assert dut.uio_out.value[5] == 0  # frame
+    assert dut.uo_out.value[7] == 0  # PWM_SET
+    assert dut.user_project.serial2parallel_data.value[9:0] == 0x3FF
+    assert dut.user_project.serial2parallel_data.value[19:10] == 1
+    assert dut.user_project.serial2parallel_data.value[29:20] == 0x3E0
+
+    for wait in range(0, 4096):
+        await helpers.n_clock(1)
+        helpers.log_outputs()
+        helpers.pwm_decode()
+
+    assert helpers.led_green == 2
+    assert helpers.led_blue == 0x3E0 * 2
+    assert helpers.led_red == 0x3FF * 2
